5411bf45f78924b009eac063b53a5b88af33c5cd
SamanehPirveisi <samaneh_pirveisy@yahoo.com>
2018-06-04 15:39:32 +0200

    adding crud method for contactdao

--- src/main/java/MainExample.java
+++ src/main/java/MainExample.java
@@ -14,22 +14,32 @@ public class MainExample {
 //		cdao.createUser(c);
 	PhoneBookDao pDao=new PhoneBookDao();
 	PhoneBook p=new PhoneBook("owner");
-//	Contact c=new Contact("ali",9999);
-//	Contact c2=new Contact("zahra",654);
-//	
+	Contact c=new Contact("ali",9999);
+	Contact c2=new Contact("zahra",654);
+//cdao.createContact(c);
+//cdao.createContact(c2);
+
 //	p.addContact(c);
 //	p.addContact(c2);
 //	c.setPhoneBook(p);
 //	c2.setPhoneBook(p);
 //	pDao.createUser(p);
 //		
-	User u=new User("sahar","far");	
-	Address a=new Address("zzzz",5);
-	u.setAddress(a);
-//	u.addPhoneBook(p);
-//	p.addUser(u);
-	UserDao uDao=new UserDao();
-	uDao.createUser(u);
+//	User u=new User("sahar","far");	
+//	Address a=new Address("zzzz",5);
+//	u.setAddress(a);
+////	u.addPhoneBook(p);
+////	p.addUser(u);
+//	UserDao uDao=new UserDao();
+//	uDao.createUser(u);
+//	Contact x=cdao.getContactById(9);
+//	cdao.deleteUser(x);
+	cdao.deleteContactByName("ali");
+//	x.setName("mahsa");
+//	cdao.updateContactById(7, "ahmad");
+//cdao.updateContact(x);
+//	System.out.println(x.getName());
+//cdao.getContactByName("ali").stream().map(m->m.getName()).forEach(System.out::println);
 	
 //	pDao.createUser(p);
 //pDao.getPhoneBookById(1).getListContact().stream().map(m-> m.getName()).forEach(System.out::println);
--- src/main/java/com/samane/dao/ContactDao.java
+++ src/main/java/com/samane/dao/ContactDao.java
@@ -1,7 +1,11 @@
 package com.samane.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
 
 import com.samane.model.Contact;
 
@@ -11,7 +15,7 @@ public class ContactDao {
 
 	// 1- Create
 
-	public boolean createUser(Contact c) {
+	public boolean createContact(Contact c) {
 		boolean res = false;
 		Session session = HibernateUtil.openSession();
 		Transaction tx = null;
@@ -34,5 +38,145 @@ public class ContactDao {
 		return res;
 
 	}
+	public Contact getContactById(long id) {
+		Contact c= null;
+		Session session = HibernateUtil.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.getTransaction();
+			tx.begin();
 
+			c=session.get(Contact.class, id);
+
+			tx.commit();
+			
+		} catch (Exception ex) {
+
+			tx.rollback();
+
+		} finally {
+			session.close();
+		}
+
+		return c;
+
+	}
+	public List<Contact> getContactByName(String name) {
+		List<Contact> list = null;
+		Session session = HibernateUtil.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.getTransaction();
+			tx.begin();
+
+			Query query=session.createQuery("from Contact where name=:var");
+			query.setString("var", name);
+             list=query.getResultList();
+			tx.commit();
+			
+		} catch (Exception ex) {
+
+			tx.rollback();
+
+		} finally {
+			session.close();
+		}
+
+		return list;
+
+	}
+	public boolean updateContact(Contact c ){
+		boolean res = false;
+		Session session = HibernateUtil.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.getTransaction();
+			tx.begin();
+
+			session.update(c);
+
+			tx.commit();
+			res = true;
+		} catch (Exception ex) {
+
+			tx.rollback();
+
+		} finally {
+			session.close();
+		}
+
+		return res;
+
+	}
+	public boolean updateContactById(long id , String name) {
+		boolean res = false;
+		Session session = HibernateUtil.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.getTransaction();
+			tx.begin();
+
+			Query query=session.createQuery("UPDATE Contact SET name=:firstname WHERE contact_id=:idnumber");
+			query.setString("firstname",name);
+			query.setLong("idnumber", id);
+		query.executeUpdate();
+			res = true;
+		} catch (Exception ex) {
+
+			tx.rollback();
+
+		} finally {
+			session.close();
+		}
+
+		return res;
+
+	}
+	public boolean deleteUser(Contact c) {
+		boolean res = false;
+		Session session = HibernateUtil.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.getTransaction();
+			tx.begin();
+
+			session.delete(c);
+
+			tx.commit();
+			res = true;
+		} catch (Exception ex) {
+
+			tx.rollback();
+
+		} finally {
+			session.close();
+		}
+
+		return res;
+
+	}
+	public boolean deleteContactByName(String name) {
+		boolean res = false;
+		Session session = HibernateUtil.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.getTransaction();
+			tx.begin();
+
+			Query query=session.createQuery("DELETE FROM Contact WHERE name=:contactname");
+			query.setString("contactname",name);
+			
+		query.executeUpdate();
+			res = true;
+		} catch (Exception ex) {
+
+			tx.rollback();
+
+		} finally {
+			session.close();
+		}
+
+		return res;
+
+	}
 }
